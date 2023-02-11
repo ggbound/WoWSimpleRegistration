b7d4545488340d48a9dbbc65b3033152bd10b73e
zhuhaibing <1104061194@qq.com>
2023-02-11 09:54:07 +0800

    调整客户端下载地址

--- template/icecrown/tpl/howtoconnect.php
+++ template/icecrown/tpl/howtoconnect.php
@@ -11,6 +11,7 @@
     <p>2. <?php elang('create_account_tip2'); ?></p>
     <p>3. <?php elang('create_account_tip3'); ?></p>
     <p>4. <?php elang('create_account_tip4'); ?></p>
+    <p>5. 客户端下载地址：链接: <a href="https://pan.baidu.com/s/1Hvef58ppZsWFjF9LIhlqxw?pwd=v3bm" target="_blank">点我下载</a> 提取码: v3bm</p>
     <p style="text-align: center;font-weight: bold;color:darkred">set
         realmlist <?php echo get_config('realmlist'); ?></p>
 </div>
