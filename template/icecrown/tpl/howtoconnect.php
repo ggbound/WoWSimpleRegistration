ed3ffb8ff5222ab8c1bf8d8868da8a1408c97ed9
zhuhaibing <1104061194@qq.com>
2023-02-17 09:19:20 +0800

    新增自动售卖NPC

--- template/icecrown/tpl/howtoconnect.php
+++ template/icecrown/tpl/howtoconnect.php
@@ -9,9 +9,6 @@
 <div class="content_box1" style="line-height: 1.5;">
     <p>1. <?php elang('create_account_tip1'); ?></p>
     <p>2. <?php elang('create_account_tip2'); ?></p>
-    <p>3. <?php elang('create_account_tip3'); ?></p>
-    <p>4. <?php elang('create_account_tip4'); ?></p>
-    <p>5. 客户端下载地址：链接: <a href="https://pan.baidu.com/s/1Hvef58ppZsWFjF9LIhlqxw?pwd=v3bm" target="_blank">点我下载</a> 提取码: v3bm</p>
-    <p style="text-align: center;font-weight: bold;color:darkred">set
-        realmlist <?php echo get_config('realmlist'); ?></p>
+    <p>3. 客户端下载地址：链接: <a href="https://pan.baidu.com/s/1Hvef58ppZsWFjF9LIhlqxw?pwd=v3bm" target="_blank">点我下载</a> 提取码: v3bm</p>
+    <p style="text-align: center;font-weight: bold;color:darkred">set realmlist <?php echo get_config('realmlist'); ?></p>
 </div>
